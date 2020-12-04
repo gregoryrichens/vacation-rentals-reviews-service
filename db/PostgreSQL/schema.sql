d7420f06afdaec0d545bd8086865c6176919c886
Gregory Richens <gregoryrichens@gmail.com>
2020-12-03 23:57:41 -0700

    postgres schema now consistent with c*

--- db/PostgreSQL/schema.sql
+++ db/PostgreSQL/schema.sql
@@ -1,31 +1,32 @@
 DROP TABLE IF EXISTS reviews;
 
 CREATE TABLE reviews (
-  id GENERATED ALWAYS AS IDENTITY,
+  id BIGINT PRIMARY KEY,
   user_id INT REFERENCES users(id) NOT NULL,
   listing_id INT REFERENCES listings(id) NOT NULL,
-  text NOT NULL,
-  cleanliness INT DEFAULT 5,
-  communication INT DEFAULT 5,
-  check_in INT DEFAULT 5,
-  accuracy INT DEFAULT 5,
-  location INT DEFAULT 5,
-  value INT DEFAULT 5,
+  text VARCHAR(20000) NOT NULL,
+  cleanliness SMALLINT DEFAULT 5,
+  communication SMALLINT DEFAULT 5,
+  check_in SMALLINT DEFAULT 5,
+  accuracy SMALLINT DEFAULT 5,
+  location SMALLINT DEFAULT 5,
+  value SMALLINT DEFAULT 5,
   date DATETIME NOT NULL,
 );
 
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-  id GENERATED ALWAYS AS IDENTITY,
+  id BIGINT PRIMARY KEY,
   username VARCHAR(50) NOT NULL,
-  first_name VARCHAR(50) NOT NULL,
-  last_name VARCHAR(50) DEFAULT NULL,
-  photo_url VARCHAR(255) DEFAULT NULL,
+  name VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  avatar_url VARCHAR(255) DEFAULT NULL,
 );
 
 DROP TABLE IF EXISTS listings;
 
 CREATE TABLE listings (
-  id GENERATED ALWAYS AS IDENTITY,
+  id BIGINT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
 );
