3a30b5889426d1cb5274e97268a8ae7c6d218af4
Gregory Richens <gregoryrichens@gmail.com>
2020-12-10 11:06:56 -0700

    minor config changes for new relic

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ReviewApp from './components/ReviewApp';
 
+require('newrelic');
+
 ReactDOM.render(<ReviewApp />, document.getElementById('service3'));
